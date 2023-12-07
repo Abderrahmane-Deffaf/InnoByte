3cf46b5b71b446f8a135676d54a15a497f543ed7
zakaryaalgeria <zz194451@gmail.com>
2023-12-07 10:22:14 +0100

    update about bytecraft and event location to be shown only when user in view

--- src/components/event-location/EventLocation.jsx
+++ src/components/event-location/EventLocation.jsx
@@ -4,70 +4,74 @@ import bg_bottom from "./../../assets/eventLocation/bg_bottom.png";
 import bg_top from "./../../assets/eventLocation/bg_top.png";
 import syphax from './../../assets/eventLocation/syphax.png'
 import shadow from "./../../assets/eventLocation/shadow.png";
+import { useInView } from "react-intersection-observer";
+
 const EventLocation = () => {
   
+  const { ref, inView } = useInView();
   
   return (
     <section className="flex flex-col overflow-hidden justify-center items-center section relative">
-      <div
-        className="text-center"
-      >
-        <h3 className="text-decoration-1 ">Event Location</h3>
-        <h1 className="text-decoration-2">Syphax Hotel</h1>
-      </div>
-      <div
-        className="absolute top-1/2 z-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[360px] h-[360px]"
-      >
-        <img
-          className={`logo`}
-          src={syphax}
-          alt="SYPHAX"
-          style={{ zIndex: 1 }}
-        />
-      </div>
+      <div ref={ref} >
         <div
-          className="box"
-          style={{ zIndex: 2 }}
+          className={` ${inView ? "text-center" : "hidden"}`}
         >
-          <div
-            className="bg-image"
-          >
-            <img
-              src={shadow}
-              className="w-full h-full"
-              alt=""
-            />
-          </div>
-          <div
-            className="box-text"
-          >
-            Syphax Hotel offers a comfortable and welcoming retreat for
-            visitors. As a 3-star establishment, it combines affordability
-            with a range of amenities. It is an ideal choice for those seeking
-            a modest yet comfortable stay in a picturesque Algerian setting.
-          </div>
+          <h3 className="text-decoration-1 ">Event Location</h3>
+          <h1 className="text-decoration-2">Syphax Hotel</h1>
         </div>
         <div
-          className="btn-div"
+          className={` ${inView ? "logo-div" : "hidden"}`}
         >
-          <button className="btn">
-              <span className="text-decoration relative z-20">
-                View location
-              </span>
-            </button>
+          <img
+            className={`logo`}
+            src={syphax}
+            alt="SYPHAX"
+            style={{ zIndex: 1 }}
+          />
+        </div>
+          <div
+            className={` ${inView ? "box" : "hidden"}`}
+            style={{ zIndex: 2 }}
+          >
+            <div
+              className="bg-image"
+            >
+              <img
+                src={shadow}
+                className="w-full h-full"
+                alt=""
+              />
+            </div>
+            <div
+              className="box-text"
+            >
+              Syphax Hotel offers a comfortable and welcoming retreat for
+              visitors. As a 3-star establishment, it combines affordability
+              with a range of amenities. It is an ideal choice for those seeking
+              a modest yet comfortable stay in a picturesque Algerian setting.
+            </div>
           </div>
+          <div
+            className={` ${inView ? "btn-div" : "hidden"} `}
+          >
+            <button className="btn">
+                <span className="text-decoration relative z-20">
+                  View location
+                </span>
+              </button>
+            </div>
+        <img
+          src={bg_top}
+          alt="top"
+          className="top-image-bg "
+        />
 
-      <img
-        src={bg_top}
-        alt="top"
-        className="top-image-bg "
-      />
-
-      <img
-        src={bg_bottom}
-        alt="button"
-        className="button-image-bg"
-      />
+        <img
+          src={bg_bottom}
+          alt="button"
+          className="button-image-bg"
+        />    
+      </div>
     </section>
   );
 };
